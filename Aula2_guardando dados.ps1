60800e590cd2297b8765abe777b8410aec038260
Jefferson Oliveira <202501069592@alunos.estacio.br>
2026-05-12 21:30:50 -0300

    Add files via upload

---
+++ Aula2_guardando dados.ps1
@@ -0,0 +1,4 @@
+﻿#Guardando dados na memoria
+$algo = Read-Host "Informe algo"
+#exibi os dados salvos na variavel
+Write-Host $algo
---
+++ Aula2_muda a cor do diretorio no terminal.ps1
@@ -0,0 +1,2 @@
+﻿#Muda a cor do diretorio no terminal, a cor coloca entre aspas duplas
+$host.UI.RawUI.BackgroundColor = "Green"
