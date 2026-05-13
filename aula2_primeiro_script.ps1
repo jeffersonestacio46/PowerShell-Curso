bd8a0b56bf352eebbdaa4fbcfa95eab1c16beea0
Jefferson Oliveira <202501069592@alunos.estacio.br>
2026-05-12 23:10:16 -0300

    Add files via upload

---
+++ aula2_primeiro_script.ps1
@@ -0,0 +1,24 @@
+﻿#crianção de script
+$backupCor = $host.ui.RawUI.BackgroundColor
+$host.UI.RawUI.BackgroundColor = "Darkgray"
+cls
+
+#posicionando o curso na coluna 2 e na linha 2
+$host.UI.RawUI.CursorPosition = New-Object System.Management.Automation.Host.Coordinates (2,2)
+Write-Host "Informe o valor...:"
+
+$host.UI.RawUI.CursorPosition = New-Object System.Management.Automation.Host.Coordinates (2,4)
+Write-Host "Informe o valor...:"
+
+$host.UI.RawUI.CursorPosition = New-Object System.Management.Automation.Host.Coordinates (18,2)
+$valor1 = Read-Host "."
+
+$host.UI.RawUI.CursorPosition = New-Object System.Management.Automation.Host.Coordinates (18,4)
+$valor2 = Read-Host "."
+
+$host.UI.RawUI.CursorPosition = New-Object System.Management.Automation.Host.Coordinates (2,6)
+Write-Host "Você digitou $valor1 $valor2"
+
+pause
+$host.UI.RawUI.BackgroundColor = $backupCor
+cls
